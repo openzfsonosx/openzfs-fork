openzfsonosx/openzfs

Upstream: Add crypto errata1 for projectquota-less datasets
There was a short Windows of 2.0 releases before rc4 where a crypto
dataset would enable projectquota but fail to start it. Add
a work-around for that issue. It is expected this commit will
be remote in the near future.

datasets with crypto will generate the proper local_mac, and will not
be able to be imported with the broken 2.0 version.

Fixed dataset should work on other platforms again.

module/zfs/dsl_crypt.c
@@ -2696,6 +2696,18 @@ spa_do_crypt_objset_mac_abd(boolean_t generate, spa_t *spa, uint64_t dsobj,
 		return (0);
 	}
 
+#ifdef __APPLE__
+	/*
+	 * Unfortunate errata case, see module/os/macos/zfs/zio_crypt.c
+	 * If portable is GOOD, but local_mac is BAD - recompute
+	 */
+	if (bcmp(portable_mac, osp->os_portable_mac, ZIO_OBJSET_MAC_LEN) == 0 &&
+	    bcmp(local_mac, osp->os_local_mac, ZIO_OBJSET_MAC_LEN) != 0) {
+		ret = zio_crypt_do_objset_hmacs_errata1(&dck->dck_key, buf,
+		    datalen, byteswap, portable_mac, local_mac);
+	}
+#endif
+
 	if (bcmp(portable_mac, osp->os_portable_mac, ZIO_OBJSET_MAC_LEN) != 0 ||
 	    bcmp(local_mac, osp->os_local_mac, ZIO_OBJSET_MAC_LEN) != 0) {
 		abd_return_buf(abd, buf, datalen);

openzfsonosx/openzfs-fork

Signed-off-by: Jorgen Lundman <lundman@lundman.net>

module/zfs/dsl_crypt.c
@@ -2694,6 +2694,22 @@ spa_do_crypt_objset_mac_abd(boolean_t generate, spa_t *spa, uint64_t dsobj,
 		return (0);
 	}
 
+#ifdef __APPLE__
+	/*
+	 * Unfortunate errata case, see module/os/macos/zfs/zio_crypt.c
+	 * If portable is GOOD, but local_mac is BAD - recompute.
+	 * We were hoping this would not be required after the work
+	 * on the incompatibilities, but users complain they can not
+	 * mount older crypted datasets.
+	 */
+	if (memcmp(portable_mac, osp->os_portable_mac,
+	    ZIO_OBJSET_MAC_LEN) == 0 &&
+	    memcmp(local_mac, osp->os_local_mac, ZIO_OBJSET_MAC_LEN) != 0) {
+		ret = zio_crypt_do_objset_hmacs_errata1(&dck->dck_key, buf,
+		    datalen, byteswap, portable_mac, local_mac);
+	}
+#endif
+
 	if (memcmp(portable_mac, osp->os_portable_mac,
 	    ZIO_OBJSET_MAC_LEN) != 0 ||
 	    memcmp(local_mac, osp->os_local_mac, ZIO_OBJSET_MAC_LEN) != 0) {